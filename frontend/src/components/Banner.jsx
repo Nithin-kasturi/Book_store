942a7988e6d763bbe5bf677a22f8b766717ae3f5
Nithin-kasturi <107863285+Nithin-kasturi@users.noreply.github.com>
2024-09-02 16:16:00 +0530

    Update Banner.jsx

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -8,9 +8,8 @@ function Banner() {
         <div  className='max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row '>
             <div className='order-2 md:order-1 w-full  md:w-1/2 mt-12 md:mt-32'>
             <div className='space-y-12'>
-                <h1 className='text-4xl font-bold'>Welcome to Book Store find your<span className='text-[#e5981f]'> thoughts </span>here!</h1>
-                <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo voluptate vitae eveniet nulla, repellendus ipsum reprehenderit laborum deserunt nam, commodi quae! Suscipit, nisi veniam. Non officia eligendi iusto nesciunt atque.</p>
-            
+                <h1 className='text-4xl font-bold'>Discover your next great read at our Bookstore – a carefully curated collection of books that brings<span className='text-[#e5981f]'> the joy of reading</span> to your fingertips. </h1>
+                <p className='text-xl'>Explore our Bookstore, where every page brings a new adventure. Find your next favorite book among our handpicked selection of literary gems. Happy reading!</p>
             </div>
     <div className="mt-6">
     <a className="bg-[#e5981f] text-[#000001] px-3 py-2 rounded-md hover:bg-white hover:text-[#e5981f] duration-300 cursor-pointer" onClick={()=>document.getElementById("my_modal_3").showModal()}>Login</a>
